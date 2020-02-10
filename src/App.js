4084239de87dabda4928c500483f6dbfac94093e
wasimkhan318 <wasim.khan318@gmail.com>
2020-02-09 16:59:09 -0800

    useQuery hook to get data very late night refactor it latter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 import "./App.css";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+const query = gql`
+  query courseTopics {
+    courseTopics {
+      name
+      id
+    }
+  }
+`;
 function App() {
-  return <div className="App"></div>;
+  const { loading, data } = useQuery(query);
+  if (loading) return <h2>loading</h2>;
+  return (
+    <div className="App">
+      {data.courseTopics.map(d => {
+        return <li>{d.name}</li>;
+      })}
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
+const httpLink = createHttpLink({
+  uri: "https://api-euwest.graphcms.com/v1/ck6e4zq9523l401dhhs8u97s0/master"
+});
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+ReactDOM.render(<ApolloApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
